2ac9db8c351e564f38413d6fc36a9bec9b05b3cb
V <veratsien@gmail.com>
2023-04-05 19:51:34 -0400

    add team; revert code format

--- frontend/lib/app_scaffold.dart
+++ frontend/lib/app_scaffold.dart
@@ -25,13 +25,10 @@ class AppScaffoldComponent extends StatefulWidget {
 }
 
 class _AppScaffoldState extends State<AppScaffoldComponent> {
-  Widget _buildLinkButton(
-      BuildContext context, String routePath, String label) {
+  Widget _buildLinkButton(BuildContext context, String routePath, String label) {
     return Container(
       decoration: BoxDecoration(
-        border: Border(
-            bottom:
-                BorderSide(width: 1, color: Theme.of(context).primaryColor)),
+        border: Border(bottom: BorderSide(width: 1, color: Theme.of(context).primaryColor)),
       ),
       child: ListTile(
         //onPressed: () {
@@ -42,14 +39,12 @@ class _AppScaffoldState extends State<AppScaffoldComponent> {
           context.go(routePath);
         },
         //child: Text(label),
-        title: Text(label,
-            style: TextStyle(color: Theme.of(context).primaryColor)),
+        title: Text(label, style: TextStyle( color: Theme.of(context).primaryColor )),
       ),
     );
   }
 
-  Widget _buildUserButton(BuildContext context, currentUserState,
-      {double width = 100, double fontSize = 13}) {
+  Widget _buildUserButton(BuildContext context, currentUserState, { double width = 100, double fontSize = 13 }) {
     if (currentUserState.isLoggedIn) {
       return SizedBox.shrink();
     }
@@ -94,9 +89,7 @@ class _AppScaffoldState extends State<AppScaffoldComponent> {
     return _buildLinkButton(context, '/login', 'Log In');
   }
 
-  Widget _buildNavButton(
-      String route, String text, IconData icon, BuildContext context,
-      {double width = 100, double fontSize = 13}) {
+  Widget _buildNavButton(String route, String text, IconData icon, BuildContext context, { double width = 100, double fontSize = 13 }) {
     return ElevatedButton(
       onPressed: () {
         context.go(route);
@@ -124,40 +117,42 @@ class _AppScaffoldState extends State<AppScaffoldComponent> {
     );
   }
 
-  Widget _buildDrawerButton(BuildContext context,
-      {double width = 100, double fontSize = 13}) {
-    return Builder(builder: (BuildContext context) {
-      return ElevatedButton(
-        onPressed: () {
-          Scaffold.of(context).openEndDrawer();
-        },
-        style: ElevatedButton.styleFrom(
-          primary: Colors.white,
-          minimumSize: Size.fromWidth(width),
-          padding: EdgeInsets.all(0),
-        ),
-        child: Container(
-          padding: EdgeInsets.only(top: 10),
-          child: Column(
-            children: <Widget>[
-              Icon(Icons.menu, color: Theme.of(context).primaryColor),
-              Text(
-                'More',
-                style: TextStyle(
-                  color: Theme.of(context).primaryColor,
-                  fontSize: fontSize,
-                ),
-              ),
-            ],
+  Widget _buildDrawerButton(BuildContext context, { double width = 100, double fontSize = 13 }) {
+    return Builder(
+      builder: (BuildContext context) {
+        return ElevatedButton(
+          onPressed: () {
+            Scaffold.of(context).openEndDrawer();
+          },
+          style: ElevatedButton.styleFrom(
+            primary: Colors.white,
+            minimumSize: Size.fromWidth(width),
+            padding: EdgeInsets.all(0),
           ),
-        ),
-      );
-    });
+          child: Container(
+            padding: EdgeInsets.only(top: 10),
+            child: Column(
+              children: <Widget>[
+                Icon(Icons.menu, color: Theme.of(context).primaryColor),
+                Text(
+                  'More',
+                  style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontSize: fontSize,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      }
+    );
   }
 
   Widget _buildDrawer(BuildContext context, var currentUserState) {
     List<Widget> columns = [];
-    if (currentUserState.hasRole('admin')) {}
+    if (currentUserState.hasRole('admin')) {
+    }
 
     return Drawer(
       child: Column(
@@ -169,11 +164,12 @@ class _AppScaffoldState extends State<AppScaffoldComponent> {
                 child: Text(''),
               ),
               IconButton(
-                  icon: Icon(Icons.close),
-                  color: Theme.of(context).primaryColor,
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  }),
+                icon: Icon(Icons.close),
+                color: Theme.of(context).primaryColor,
+                onPressed: () {
+                  Navigator.of(context).pop();
+                }
+              ),
             ],
           ),
           _buildLinkButton(context, '/home', 'Home'),
@@ -189,89 +185,95 @@ class _AppScaffoldState extends State<AppScaffoldComponent> {
     List<Widget> rows = [
       Expanded(
         flex: 1,
-        child: _buildNavButton('/home', 'Home', Icons.home, context,
-            width: double.infinity, fontSize: 10),
+        child: _buildNavButton('/home', 'Home', Icons.home, context, width: double.infinity, fontSize: 10),
       ),
     ];
     if (!currentUserState.isLoggedIn) {
       rows.add(Expanded(
         flex: 1,
-        child: _buildUserButton(context, currentUserState,
-            width: double.infinity, fontSize: 10),
+        child: _buildUserButton(context, currentUserState, width: double.infinity, fontSize: 10),
       ));
     }
     rows.add(
       Expanded(
         flex: 1,
-        child:
-            _buildDrawerButton(context, width: double.infinity, fontSize: 10),
+        child: _buildDrawerButton(context, width: double.infinity, fontSize: 10),
       ),
     );
 
     return SafeArea(
-        child: Container(
-      height: 55,
-      child: Row(children: <Widget>[
-        ...rows,
-      ]),
-      decoration: BoxDecoration(boxShadow: [
-        BoxShadow(
-          color: Colors.grey.shade300,
-          spreadRadius: 2,
-          blurRadius: 4,
-          offset: Offset(0, 0),
-        )
-      ]),
-    ));
+      child: Container(
+        height: 55,
+        child: Row(
+          children: <Widget>[
+            ...rows,
+          ]
+        ),
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.shade300,
+              spreadRadius: 2,
+              blurRadius: 4,
+              offset: Offset(0, 0),
+            )
+          ]
+        ),
+      )
+    );
   }
 
-  Widget _buildBody(BuildContext context, var currentUserState,
-      {bool header = false}) {
+  Widget _buildBody(BuildContext context, var currentUserState, { bool header = false }) {
     if (header) {
       return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            _buildHeader(context, currentUserState),
-            // For drop shadow, otherwise it is cut off.
-            SizedBox(height: 5),
-            Expanded(
-                child: Container(
-                    color: Colors.white,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image:
-                              AssetImage('assets/images/greencity-light.png'),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      child: Align(
-                          alignment: Alignment.center,
-                          child: Container(
-                            width: 1200,
-                            child: widget.body,
-                            color: Color.fromRGBO(255, 255, 255, 0.8),
-                          )),
-                    ))),
-          ]);
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          _buildHeader(context, currentUserState),
+          // For drop shadow, otherwise it is cut off.
+          SizedBox(height: 5),
+          Expanded(
+            child: Container(
+              color: Colors.white,
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/greencity-light.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Container(
+                    width: 1200,
+                    child: widget.body,
+                    color: Color.fromRGBO(255,255,255,0.8),
+                  )
+                ),
+              )
+            )
+          ),
+        ]
+      );
     }
     return Container(
-        color: Colors.white,
-        child: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/greencity-light.png'),
-              fit: BoxFit.cover,
-            ),
+      color: Colors.white,
+      child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/greencity-light.png'),
+            fit: BoxFit.cover,
           ),
-          child: Align(
-              alignment: Alignment.center,
-              child: Container(
-                width: 1200,
-                child: widget.body,
-                color: Color.fromRGBO(255, 255, 255, 0.8),
-              )),
-        ));
+        ),
+        child: Align(
+          alignment: Alignment.center,
+          child: Container(
+            width: 1200,
+            child: widget.body,
+            color: Color.fromRGBO(255,255,255,0.8),
+          )
+        ),
+      )
+    );
   }
 
   Widget _buildSmall(BuildContext context, var currentUserState) {
@@ -288,14 +290,13 @@ class _AppScaffoldState extends State<AppScaffoldComponent> {
         backgroundColor: Colors.white,
         title: GestureDetector(
           child: Image.asset('assets/images/logo.png', width: 100, height: 50),
-          onTap: () => context.go(Routes.home),
+          onTap: ()=> context.go(Routes.home),
         ),
         actions: <Widget>[
           //_buildNavButton('/home', 'Home', Icons.home, context),
           _buildNavButton('/blog', 'Blog', Icons.description, context),
           _buildNavButton('/design', 'Design', Icons.hive, context),
-          _buildNavButton(
-              '/lend-library', 'Lend Library', Icons.build, context),
+          _buildNavButton('/lend-library', 'Lend Library', Icons.build, context),
           _buildNavButton('/team', 'Team', Icons.diversity_3, context),
           //_buildUserButton(context, currentUserState),
           _buildDrawerButton(context),
@@ -309,12 +310,14 @@ class _AppScaffoldState extends State<AppScaffoldComponent> {
   @override
   Widget build(BuildContext context) {
     var currentUserState = context.watch<CurrentUserState?>();
-    return LayoutBuilder(builder: (context, constraints) {
-      if (constraints.maxWidth > 600) {
-        return _buildMedium(context, currentUserState);
-      } else {
-        return _buildSmall(context, currentUserState);
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > 600) {
+          return _buildMedium(context, currentUserState);
+        } else {
+          return _buildSmall(context, currentUserState);
+        }
       }
-    });
+    );
   }
 }
--- frontend/lib/modules/team.dart
+++ frontend/lib/modules/team.dart
@@ -9,19 +9,81 @@ class Team extends StatefulWidget {
 
 class _TeamState extends State<Team> {
 
-  @override
-  void initState() {
-    super.initState();
-  }
+ Map<String, dynamic> _membersImage = {
+   'Isak': {
+     'imageUrl': 'images/people/isak.jpeg',
+     'bio': 'placeholder placeholder placeholder placeholder placeholder',
+   },
+   'Lora': {
+     'imageUrl': 'images/people/lora.jpeg',
+     'bio': 'placeholder',
+   },
+   'Luke': {
+     'imageUrl': 'images/people/luke.jpeg',
+     'bio': 'placeholder',
+   },
+   'Vera': {
+     'imageUrl': 'images/people/vera.jpeg',
+     'bio':'placeholder',
+   },
+ };
+
 
   @override
   Widget build(BuildContext context) {
     return AppScaffoldComponent(
-      body: ListView(
-        children: [
-          Text('Team'),
-        ]
+      body: Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: _memberBuilder(),
+        ),
       )
     );
+  }
+
+  List<Widget> _memberBuilder(){
+    List<Widget> _members = [];
+    _membersImage.forEach((name, meta) {
+      Widget memberBox = Container(
+        constraints: BoxConstraints(
+          maxWidth: 200,
+        ),
+        child: Column(
+          children: [
+        SizedBox(
+          width: 200,
+            height: 200,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+                child: Image(image: AssetImage(meta['imageUrl']))
+            )
+        ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.black.withAlpha(12),
+                  borderRadius: BorderRadius.circular(5)
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Text(name),
+                      Text(meta['bio']),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      );
+
+      _members.add(memberBox);
+    });
+
+
+    return _members;
   }
 }
